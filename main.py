6a6c8d112670f97c221a0097eab20a09bff3d7b8
crab1224 <mulran1224@gmail.com>
2025-03-17 13:42:47 +0900

    최종

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-print('Hello Mars')
-f = open("mission_computer_main.log", "r")
-print(f.read())
+try:
+    print('Hello Mars')
+    f = open("mission_computer_main.log", "r")
+    print(f.read())
+except Exception as e:
+    print("예상치못한 오류 발생");
+except FileNotFoundError as e:
+    print("파일을 찾을 수 없음 ");
+except IOError as e: 
+    print("입출력 오류 발생");
+
+
+
