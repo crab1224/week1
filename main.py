9979c999d36d43b5a6dfac45cdc2337fc3f441a7
crab1224 <mulran1224@gmail.com>
2025-03-19 12:15:27 +0900

    Last

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+
 try:
     print('Hello Mars')
-    f = open("mission_computer_main.log", "r")
+    f = open("mission_computer_main.log", "r", encoding= 'UTF-8')
     print(f.read())
+    f.close()
 except Exception as e:
-    print("예상치못한 오류 발생");
+    print("예상치못한 오류 발생")
 except FileNotFoundError as e:
-    print("파일을 찾을 수 없음 ");
+    print("파일을 찾을 수 없음 ")
 except IOError as e: 
-    print("입출력 오류 발생");
+    print("입출력 오류 발생")
+
+
 
 
 
